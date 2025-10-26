3f1766bf1984bf0ca61a380d7619ebd3ae2448d3
aleparty82 <aleparty82@gmail.com>
2025-10-26 08:24:28 +0100

    1.1.0
    Interfaccia grafica

--- networkspeed.py
+++ networkspeed.py
@@ -1,6 +1,9 @@
 import speedtest
 import tkinter as tk
 
+dl_var = None
+ul_var = None
+ping_var = None
 
 def mbit_conversion(bits):
     return bits / 1024 / 1024
@@ -14,24 +17,59 @@ def test_speed():
     #print(f"Download: {dl:.1f} Mb/s\nUpload: {ul:.1f} Mb/s\nPing: {ping:.0f} ms")
     return dl, ul, ping
 
-if __name__ == "__main__":
+def refresh_values():
+    global dl_var, ul_var, ping_var
+    
     dl, ul, ping = test_speed()
-    print(f"Download: {dl:.1f} Mb/s\nUpload: {ul:.1f} Mb/s\nPing: {ping:.0f} ms")
+    
+    dl_var.set(f"Download Speed: {dl:.1f} Mb/s")
+    ul_var.set(f"Upload Speed: {ul:.1f} Mb/s")
+    ping_var.set(f"Ping: {int(ping)} ms")
+
+    #print(f"Download: {dl:.1f} Mb/s\nUpload: {ul:.1f} Mb/s\nPing: {ping:.0f} ms")
+
+if __name__ == "__main__":
+    #dl, ul, ping = test_speed()
+    #print(f"Download: {dl:.1f} Mb/s\nUpload: {ul:.1f} Mb/s\nPing: {ping:.0f} ms")
     
     root = tk.Tk()
     root.title("Speed Test")
-    root.geometry("600x400")
-    etichetta = tk.Label(
+    root.geometry("300x150")
+    #root.attributes('-topmost', True)
+    
+    dl_var = tk.StringVar(value="Download Speed: N/A")
+    ul_var = tk.StringVar(value="Upload Speed: N/A")
+    ping_var = tk.StringVar(value="Ping: N/A")
+
+    bottone_avvio = tk.Button(
         master=root,
-        text=f"Download: {dl:.1f} Mb/s\nUpload: {ul:.1f} Mb/s\nPing: {ping:.0f} ms",
+        text="Avvia lo Speed Test",
+        command=refresh_values
+    ).pack(padx=5, pady=5)
+
+    etichetta_dl = tk.Label(
+        master=root,
+        textvariable=dl_var,
         font=("Arial", 16),
         justify=tk.LEFT
-    )
-    etichetta.pack(
-        padx=50,
-        pady=50
-    )
-    
+    ).pack(padx=5, pady=5)
+
+    etichetta_ul = tk.Label(
+        master=root,
+        textvariable=ul_var,
+        font=("Arial", 16),
+        justify=tk.LEFT
+    ).pack(padx=5, pady=5)
+
+    etichetta_ping = tk.Label(
+        master=root,
+        textvariable=ping_var,
+        font=("Arial", 16),
+        justify=tk.LEFT
+    ).pack(padx=5, pady=5)
+
+    #root.lift()
+    root.focus_force()
     root.mainloop()
 
 
